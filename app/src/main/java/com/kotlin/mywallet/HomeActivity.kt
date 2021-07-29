b93fdbb711646c0f8f9838061441aae67f8c3fa2
Angel Alvarado <angel.alvarado.dev@gmail.com>
2021-07-28 20:01:31 -0500

    Se mejoraron las vistas y se agregaron las activities de cada una

--- app/src/main/java/com/kotlin/mywallet/HomeActivity.kt
+++ app/src/main/java/com/kotlin/mywallet/HomeActivity.kt
@@ -1,11 +1,39 @@
 package com.kotlin.mywallet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var welcomeHomeTxt: TextView
+    private lateinit var totalAmountTxt: TextView
+    private lateinit var addIncomeButton: Button
+    private lateinit var addExpenseButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        addIncomeButton = findViewById(R.id.addIncomeButton)
+        addExpenseButton = findViewById(R.id.addExpenseButton)
+
+        addIncomeButton.setOnClickListener(addChargeIncome())
+        addExpenseButton.setOnClickListener(addChargeExpensive())
+    }
+
+    private fun addChargeIncome() = View.OnClickListener { view ->
+        val intent = Intent(this@HomeActivity, AddChargeActivity::class.java)
+        intent.putExtra("chargeType", "income")
+        startActivity(intent)
+    }
+
+    private fun addChargeExpensive() = View.OnClickListener { view ->
+        val intent = Intent(this@HomeActivity, AddChargeActivity::class.java)
+        intent.putExtra("chargeType", "expense")
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/kotlin/mywallet/MainActivity.kt
+++ app/src/main/java/com/kotlin/mywallet/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.kotlin.mywallet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var signinBtn: Button
+    private lateinit var emailTxt: EditText
+    private lateinit var passwordTxt: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        signinBtn = findViewById(R.id.loginButton)
+
+
+        signinBtn.setOnClickListener(onSignin())
+    }
+
+
+    private fun onSignin() = View.OnClickListener { view ->
+        emailTxt = findViewById(R.id.editTextEmailAddress)
+        passwordTxt = findViewById(R.id.editTextPassword)
+        if (emailTxt.text.toString().equals("prueba@email.com") && passwordTxt.text.toString().equals("password")) {
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            intent.putExtra("email", emailTxt.text)
+            startActivity(intent)
+        }
+        else
+        {
+            Toast.makeText(
+                this,
+                "Wrong email or password",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }

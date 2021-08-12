ec247088bcd1ad1d9452589c44a078cc37a1a451
Jellian <haanniiaarodriguez@gmail.com>
2021-08-12 17:07:15 -0500

    se añaden nuevos activities acerca de & aviso de privacidad
    Signed-off-by: Jellian <haanniiaarodriguez@gmail.com>

---
+++ app/src/main/java/com/kotlin/mywallet/AboutOf.kt
@@ -0,0 +1,11 @@
+package com.kotlin.mywallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutOf : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_of)
+    }
+}
---
+++ app/src/main/java/com/kotlin/mywallet/AvisoPrivacidad.kt
@@ -0,0 +1,11 @@
+package com.kotlin.mywallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AvisoPrivacidad : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aviso_privacidad)
+    }
+}
